ed467a14a0fa7d68a6d3b516dd350e6c4cda1ab1
Ezechiel Gnepa <ezechiel.gnepa09@gmail.com>
2016-10-28 21:48:26 -0400

    installation de l'environnement

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name interTechApp
+ * @description
+ * # interTechApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('interTechApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name interTechApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the interTechApp
+ */
+angular.module('interTechApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
