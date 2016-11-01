236c7c60ade633a555fa4c7c46fc97aca78c66ed
Ezechiel Gnepa <ezechiel.gnepa09@gmail.com>
2016-11-01 17:29:32 -0400

    initialisation du projet

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2016-10-29 using generator-angular 0.15.1
+// Generated on 2016-11-01 using generator-angular 0.15.1
 'use strict';
 
 // # Globbing
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,24 @@
  * Main module of the application.
  */
 angular
-  .module('interTechApp', []);
+  .module('interTechApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
