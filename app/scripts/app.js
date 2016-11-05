8c493f8e370d80e463141ba53d68467aec041bf9
Ezechiel Gnepa <ezechiel.gnepa09@gmail.com>
2016-11-05 04:35:45 -0400

    mis en place du font end

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,24 +9,24 @@
  * Main module of the application.
  */
 angular
-  .module('interTechApp', [
-    'ngAnimate',
-    'ngResource',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('interTechApp', [
+        'ngAnimate',
+        'ngResource',
+        'ngRoute'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
