5b08d812d9019cc2821877724e22d3d0467e0a2d
limonsso <ezechiel.gnepa09@gmail.com>
2016-12-20 21:44:52 -0500

    dev accueil

---
+++ app/scripts/controllers/QSNController.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('interTechApp')
+    .controller('QuiSommesNousCtl', ['$scope', function ($scope) {
+        $scope.template = {name: 'QuiSommesNous', url: 'views/QuiSommesNous.html'}
+    }])
